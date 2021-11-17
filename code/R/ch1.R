2b671f92c6e20b2064321b50742cb70d52bde9ee
rhcaeb <ryan.beach@ontario.ca>
2021-11-17 11:37:25 -0500

    complete first few subsections of ch1 for both py and R

--- code/R/ch1.R
+++ code/R/ch1.R
@@ -20,3 +20,16 @@ median(state[['Population']]) # median population
 
 weighted.mean(state[['Murder.Rate']], w = state[['Population']])
 weightedMedian(state[['Murder.Rate']], w = state[['Population']]) # matrixStats() function
+
+### e.g. variability estimates of State population
+sd(state[['Population']]) # standard deviation
+IQR(state[['Population']]) # interquartile range
+mad(state[['Population']]) # median absolute deviation
+
+### e.g. percentiles
+quantile(state[['Murder.Rate']], p = c(0.05, 0.25, 0.50, 0.75, 0.95))
+
+### e.g. boxplot
+boxplot(state[['Population']]/1000000, ylab = 'Population (millions)')
+
+### frequency tables and histograms
--- code/python/ch1.py
+++ code/python/ch1.py
@@ -1,4 +1,53 @@
 # Chapter 1 - Exploratory Data Analysis
 
 ## packages ----
-print("Hello World")
+%matplotlib inline
+
+from pathlib import Path
+
+import pandas as pd
+import numpy as np
+from scipy.stats import trim_mean
+from statsmodels import robust
+
+import seaborn as sns
+import matplotlib.pylab as plt
+
+import wquantiles
+
+import os
+# os.getcwd()
+# os.chdir('')
+
+## estimates of location ----
+
+## import data
+state = pd.read_csv('data/state.csv')
+print(state.head(5))
+
+state['Population'].mean()
+trim_mean(state['Population'], 0.1)
+state['Population'].median()
+print(np.average(state['Murder.Rate'], weights=state['Population']))
+print(wquantiles.median(state['Murder.Rate'], weights=state['Population']))
+
+## estimates of variability ----
+state['Population'].std() # standard deviation
+state['Population'].quantile(0.75) - state['Population'].quantile(0.25) # IQR
+robust.scale.mad(state['Population']) # median absolute deviation
+
+## exploring data distribution ----
+### percentiles
+state['Murder.Rate'].quantile([0.05, 0.25, 0.50, 0.75, 0.95])
+
+### boxplot
+ax = (state['Population']/1_000_000).plot.box()
+ax.set_ylabel('Population (millions)')
+
+## frequency tables and histograms ----
+binnedPopulation = pd.cut(state['Population'], 10)
+binnedPopulation.value_counts()
+
+### histogram
+ax = (state['Population'] / 1_000_000).plot.hist(figsize = (4, 4))
+ax.set_xlabel('Population (millions)')
